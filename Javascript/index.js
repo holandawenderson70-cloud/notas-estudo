eb98b66f555512e96de50905e2e41ff2774e7cad
Wenderson <holandawenderson70@gmail.com>
2025-10-30 22:12:43 -0300

    finalizando a aula

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,2 +1,13 @@
 console.log("fuscão preto")
 
+// criando variavel
+// declarando variavel e atribuindo 102
+let numero = 102
+console.log(numero)
+numero = 99
+console.log (numero)
+
+const nome = "Wenderson"
+console.log(nome)
+// nome = "batata" - nao pode atribuir valor para
+//  constante depois de declara
