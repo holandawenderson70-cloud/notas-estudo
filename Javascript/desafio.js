3d93e6f4534c07d345368f51e4fa570f7ff85209
wenderson <holandawenderson70@gmail.com>
2025-11-07 21:24:02 -0300

    desafios

---
+++ Javascript/desafio.js
@@ -0,0 +1,6 @@
+const idade = 20
+const ano = 12
+const mes = 30
+const resultado = idade * ano * mes
+const anos = idade * ano
+console.log("uma pessoa de 20 anos tem " +anos+ " mês e " +resultado+ " dias de vida")
---
+++ Javascript/transforma.js
@@ -0,0 +1,6 @@
+const numr1 = 1.8
+const numr2 = 32
+const numr3 = 20
+const resultado = numr1 * numr3 + numr2
+
+console.log ("transfira o 1,8 celso para 32 fahrenheit" +resultado)
